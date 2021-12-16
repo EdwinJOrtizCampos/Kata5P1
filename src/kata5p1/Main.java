d0dbff6006f235cd866314fd8a068b92b4743e4a
EdwinJOrtizCampos <edwinjavierortizcamposORCA1@HOTMAIL.COM>
2021-12-16 08:22:48 +0000

    Primera versión de Kata5P1

---
+++ src/kata5p1/Main.java
@@ -0,0 +1,40 @@
+package kata5p1;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class Main {
+    public static void main(String[] args) {
+        connect();
+    }
+    private static void connect() {
+        Connection conn = null;
+        try {
+            String url = "jdbc:sqlite:KATA5.db";
+            conn = DriverManager.getConnection(url);
+            String sql = "SELECT * FROM PEOPLE";
+            try (Connection conn = this.connect();
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(sql)){
+                while (rs.next()) {
+                    System.out.println(rs.getInt("id") + "\t" +
+                    rs.getString("Name") + "\t" +
+                    rs.getString("Apellidos") + "\t" +
+                    rs.getString("Departamento") + "\t");
+                }
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+}
